2a7a44763d2d3d6d38f46abaa1b78b7a3c6015c5
Clarion Heard <clarion.heard@gmail.com>
2020-01-06 21:19:14 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,56 +2,17 @@
 require 'yaml'
 
 def load_library(file_path)
-  hash = {
-    :get_meaning => {
-      "☜(⌒▽⌒)☞" => "angel",
-      "ヽ(ｏ`皿′ｏ)ﾉ" => "angry",
-      "(ΘεΘ;)" => "bored",
-      "(゜.゜)" => "confused",
-      "(#^.^#)" => "embarrassed",
-      ">゜))))彡" => "fish",
-      "(^0_0^)" => "glasses",
-      "(￣ー￣)" => "grinning",
-      "(＾ｖ＾)" => "happy",
-      "(*^3^)/~☆" => "kiss",
-      "(Ｔ▽Ｔ)" => "sad",
-      "o_O" => "surprised",
-      "(^_-)" => "wink"
-    },
-    :get_emoticon => {
-      
-    }
+  result = {}
+  dictionary = YAML.load_file(file_path)
+  result[:get_meaning] = {}
+  result[:get_emoticon] = {}
+  dictionary.each {|key, value_array|
+    result[:get_meaning][value_array[1]] = key;
+    result[:get_emoticon][value_array[0]] = value_array[1]
   }
-
-
-
-    it "the keys inside the 'get_emoticon' hash are the English emoticons" do
-      emoticons = ["O:)", ":'(", ";)", ":$"]
-      emoticons.each do |english_emoticon|
-        if result['get_emoticon'] then
-          expect(result['get_emoticon'].keys).to include(english_emoticon)
-        else
-          expect(result[:get_emoticon].keys).to include(english_emoticon)
-        end
-      end
-    end
-
-    it "the emoticon keys inside the 'get_emoticon' hash point to their Japanese equivalents" do
-      emoticons = {"O:)" => "☜(⌒▽⌒)☞", ":'(" => "(Ｔ▽Ｔ)", ";)" => "(^_-)"}
-      emoticons.each do |english_emoticon,japanese_emoticon|
-        if result['get_emoticon'] then
-          expect(result['get_emoticon'][english_emoticon]).to eq(japanese_emoticon)
-        else
-          expect(result[:get_emoticon][english_emoticon]).to eq(japanese_emoticon)
-        end
-      end
-    end
-
-  end
-
+pp result
 end
-  # code goes here
-end
+
 
 def get_japanese_emoticon
   # code goes here
