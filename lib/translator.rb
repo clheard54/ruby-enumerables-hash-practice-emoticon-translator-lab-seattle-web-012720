07e86eea5e27bd10ff7f5fccc2e7967c07234e69
Clarion Heard <clarion.heard@gmail.com>
2020-01-06 20:57:48 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,65 @@
 # require modules here
 require 'yaml'
 
-
-
 def load_library(file_path)
   hash = {
-    :get_meaning => 
-    :get_emoticon
+    :get_meaning => {},
+    :get_emoticon => {}
   }
+
+
+    it "the keys inside the 'get_meaning' hash are the Japanese emoticons" do
+      emoticons = ["☜(⌒▽⌒)☞", "(ΘεΘ;)", "(￣ー￣)", "o_O", "(^_-)"]
+      emoticons.each do |japanese_emoticon|
+        if result['get_meaning'] then
+          expect(result['get_meaning'].keys).to include(japanese_emoticon)
+        else
+          expect(result[:get_meaning].keys).to include(japanese_emoticon)
+        end
+      end
+    end
+
+    it "the emoticon keys inside the 'get_meaning' hash point to their meanings" do
+      emoticons = {
+        "☜(⌒▽⌒)☞" => "angel",
+        "(ΘεΘ;)" => "bored",
+        "o_O" => "surprised", 
+        "(^_-)" => "wink"
+      }
+      emoticons.each do |japanese_emoticon,meaning|
+        if result['get_meaning'] then
+          expect(result['get_meaning'][japanese_emoticon]).to eq(meaning)
+        else
+          expect(result[:get_meaning][japanese_emoticon]).to eq(meaning)
+        end
+      end
+    end
+
+    it "the keys inside the 'get_emoticon' hash are the English emoticons" do
+      emoticons = ["O:)", ":'(", ";)", ":$"]
+      emoticons.each do |english_emoticon|
+        if result['get_emoticon'] then
+          expect(result['get_emoticon'].keys).to include(english_emoticon)
+        else
+          expect(result[:get_emoticon].keys).to include(english_emoticon)
+        end
+      end
+    end
+
+    it "the emoticon keys inside the 'get_emoticon' hash point to their Japanese equivalents" do
+      emoticons = {"O:)" => "☜(⌒▽⌒)☞", ":'(" => "(Ｔ▽Ｔ)", ";)" => "(^_-)"}
+      emoticons.each do |english_emoticon,japanese_emoticon|
+        if result['get_emoticon'] then
+          expect(result['get_emoticon'][english_emoticon]).to eq(japanese_emoticon)
+        else
+          expect(result[:get_emoticon][english_emoticon]).to eq(japanese_emoticon)
+        end
+      end
+    end
+
+  end
+
+end
   # code goes here
 end
 
